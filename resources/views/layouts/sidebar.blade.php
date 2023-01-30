6faf6bc73c7fc6a39ee3772bde35e2e892dc8f43
AzuzanAnantaF <109930500+AzuzanAnantaF@users.noreply.github.com>
2023-01-30 08:02:00 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="{{route('dashboard')}}">laundry bunda bidah</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="{{url('/dashboard')}}">jujan</a>
+      </div>
+      <ul class="sidebar-menu">
+          {{-- link --}}
+          @yield('link')
+
+        </ul>
+
+        
+    </aside>
+  </div>
